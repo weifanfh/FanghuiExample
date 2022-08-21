8e3073648d4e0a1be9a9e4193dfb22f2e0e933a6
fanghui <3142361906@qq.com>
2022-08-21 18:13:09 +0800

    关于PyCharm的断点调试debug功能的分析了解

---
+++ debuge/debuge_source.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+def debug():
+    def info():
+        for i in range(20):
+            s1=i
+            for j in range(20):
+                s2=j
+            s3=[s1,s2]
+        return s3
+    info()
